8b5fc8609016b5c1a140400a78209b79c9d03760
Virl <virlof@gmail.com>
2015-05-25 20:27:05 +0300

    Initial example how to use Underdark.

---
+++ examples/Simple/app/src/main/java/io/underdark/example/MainActivity.java
@@ -0,0 +1,112 @@
+package io.underdark.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.EnumSet;
+
+import ch.qos.logback.classic.android.BasicLogcatConfigurator;
+import io.underdark.api.Link;
+import io.underdark.api.Transport;
+import io.underdark.api.TransportKind;
+import io.underdark.api.TransportListener;
+import io.underdark.api.Underdark;
+
+
+public class MainActivity extends Activity implements TransportListener
+{
+	private static Logger logger = LoggerFactory.getLogger(MainActivity.class);
+
+	private Transport transport;
+
+	private void configureLogging()
+	{
+		BasicLogcatConfigurator.configureDefaultContext();
+
+		/*final LogConfigurator configurator = new LogConfigurator();
+
+		configurator.setUseLogCatAppender(true);
+		configurator.setUseFileAppender(false);
+		configurator.setImmediateFlush(true);
+		configurator.configure();*/
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		configureLogging();
+
+		logger.error("foo");
+		logger.debug("onCreate");
+
+		this.transport =
+		Underdark.configureTransport(
+				42,
+				this,
+				null,
+				this,
+				EnumSet.of(TransportKind.WIFI)
+		);
+
+		this.transport.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings)
+		{
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	//region TransportListener
+	@Override
+	public void transportNeedsActivity(Transport transport, ActivityCallback activityCallback)
+	{
+		activityCallback.accept(this);
+	}
+
+	@Override
+	public void transportLinkConnected(Transport transport, Link link)
+	{
+		logger.debug("connected");
+	}
+
+	@Override
+	public void transportLinkDisconnected(Transport transport, Link link)
+	{
+		logger.debug("disconnected");
+	}
+
+	@Override
+	public void transportLinkDidReceiveFrame(Transport transport, Link link, byte[] bytes)
+	{
+		logger.debug("frame");
+	}
+	//endregion
+} // MainActivity
